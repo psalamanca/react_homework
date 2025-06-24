9e565b568669686133e4423953e75593db068e79
Clopez <cristianlopez8x3@gmail.com>
2025-06-23 22:59:47 -0500

    Creando mi primer boton en react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,15 +7,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Cristian Lopez</h1>
+      <h1 className='p-10'>Cristian Lopez</h1>
       <Button/>
     </>
   )
--- vite-project/src/components/Button.jsx
+++ vite-project/src/components/Button.jsx
@@ -1,5 +1,11 @@
 export function Button () {
+
+  function handleClick () {
+    console.log("Mi primer Boton")
+    alert("Mi primer Boton")
+  }
+
   return(
-    <button className="text-black">Register</button>
+    <button className=" border-solid px-5 py-5 border-1 rounded-xl text-white hover:bg-green-800" onClick={handleClick}>Register</button>
   )
 }
