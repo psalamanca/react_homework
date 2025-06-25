4ff2b20449bd37c1b4b965f79789638734cb3506
Julian Salamanca <pjulian.sb@gmail.com>
2025-06-24 22:10:20 -0500

    Add students folders

---
+++ src/app/david/comments.md
@@ -0,0 +1,1 @@
+-- En esta carpeta se agregan los componentes trabajados.
---
+++ src/app/julian/page.js
@@ -0,0 +1,10 @@
+import Label from './components/Label';
+
+export default function Welcome() {
+  return (
+    <div>
+      <h2>Bienvenidos al curso intensivo de React</h2>
+      <Label message="Aprendiendo React!" />
+    </div>
+  );
+}
